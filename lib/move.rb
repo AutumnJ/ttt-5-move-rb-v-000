0c36f634df6fc52665defde8156bd65cf2954f8c
Autumn Jung <autumnkangas@gmail.com>
2018-01-03 15:07:30 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,9 +7,9 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index(integer)
-  integer.to_i
-  index = integer - 1
+def input_to_index(input)
+  input.to_i
+  input - 1
 end
 
 def move(board, integer, character = "X")
